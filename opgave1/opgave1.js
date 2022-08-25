ba10be01f5ac84372973f6396f357d7db8d6d7b3
silke <silk0089@stud.kea.dk>
2022-08-25 11:37:21 +0200

    ændringer5

--- opgave1/opgave1.js
+++ opgave1/opgave1.js
@@ -3,12 +3,11 @@ snaps.addEventListener("click", tjek);
 
 document.querySelector("#soda").addEventListener("click", tjek);
 document.querySelector("#cola").addEventListener("click", tjek);
-document.querySelector("#ol").addEventListener("click", tjek);
-document.querySelector("#snaps").addEventListener("click", tjek);
+document.querySelector("#øl").addEventListener("click", tjek);
 
 function tjek() {
   console.log(this.alt);
-  if ((this.alt = "ol" || this.alt == "snaps")) {
+  if (this.alt == "øl" || this.alt == "snaps") {
     console.log("indeholder alkohol");
   } else {
     console.log("alkoholfri");
